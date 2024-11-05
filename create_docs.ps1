b485027b44aeb82cdef94f27b80429b833c905c2
hypo69 <181187165+hypo69@users.noreply.github.com>
2024-11-05 02:12:27 +0200

    checkpoint

--- create_docs.ps1
+++ create_docs.ps1
@@ -1,5 +1,7 @@
+# Генерация документации с помощью sphinx-apidoc
 sphinx-apidoc -o docs src --force --separate
-# Переходим в директорию docs/sphinx
+
+# Переходим в директорию docs
 Set-Location -Path "docs"
 
 # Перезаписываем index.rst, добавляя базовые директивы
@@ -14,3 +16,8 @@ Get-ChildItem -Filter *.rst | Where-Object { $_.Name -ne "index.rst" } | ForEach
     "   $($_.BaseName)" | Out-File -FilePath "index.rst" -Encoding UTF8 -Append
 }
 
+# Возвращаемся в корневую директорию проекта
+Set-Location -Path ".."
+
+# Создание HTML-документации
+sphinx-build -b html docs docs/_build/html
