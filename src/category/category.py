00cc2929a045bf207260f8f2bc51ffeeaefb42dc
hypo69 <181187165+hypo69@users.noreply.github.com>
2024-12-05 23:27:29 +0200

    AI improved code

--- src/category/category.py
+++ src/category/category.py
@@ -2,37 +2,18 @@
 # -*- coding: utf-8 -*-\
 #! venv/Scripts/python.exe
 #! venv/bin/python/python3.12
-
-"""
-.. module: src.category 
-	:platform: Windows, Unix
-	:synopsis: Module for working with categories, primarily for PrestaShop.
-
-"""
-
-import asyncio
-from pathlib import Path
-import os
-from typing import Dict
-from lxml import html
-import requests
-
-import header
-from src import gs
-from src.logger import logger
-from src.utils.jjson import j_loads, j_dumps
-from src.utils.string import StringFormatter
-from src.endpoints.prestashop import PrestaShop, PrestaCategory
-
-
-## \file hypotez/src/category/category.py
-# -*- coding: utf-8 -*-
 """
 Module for working with categories, primarily for PrestaShop.
 ============================================================
 
 This module provides classes for interacting with and
 processing product category data, particularly relevant for PrestaShop.
+
+```rst
+.. module: src.category 
+	:platform: Windows, Unix
+	:synopsis: Module for working with categories, primarily for PrestaShop.
+```
 """
 
 import asyncio
@@ -46,7 +27,6 @@ import header
 from src import gs
 from src.logger import logger
 from src.utils.jjson import j_loads, j_dumps
-from src.utils.string import StringFormatter
 from src.endpoints.prestashop import PrestaShop, PrestaCategory
 
 
@@ -117,7 +97,7 @@ class Category(PrestaCategory):
 
             return category
         except Exception as ex:
-            logger.error(f"An error occurred during category crawling: ",ex)
+            logger.error(f"An error occurred during category crawling: ", ex)
             return category
 
     def crawl_categories(self, url, depth, driver, locator, dump_file, id_category_default, category={}):
@@ -163,7 +143,7 @@ class Category(PrestaCategory):
             j_dumps(category, dump_file)
             return category
         except Exception as ex:
-            logger.error(f"An error occurred during category crawling: ",ex)
+            logger.error(f"An error occurred during category crawling: ", ex)
             return category
 
     def _is_duplicate_url(self, category, url):
@@ -184,226 +164,9 @@ def compare_and_print_missing_keys(current_dict, file_path):
     try:
         data_from_file = j_loads(file_path)
     except Exception as ex:
-        logger.error(f"Error loading data from file: ",ex)
+        logger.error(f"Error loading data from file: ", ex)
         return  # Or raise the exception
-
 
     for key in data_from_file:
         if key not in current_dict:
             print(key)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# """
-# .. module: src.category 
-# 	:platform: Windows, Unix
-# 	:synopsis: Модуль работы с категориями 
-# На сегодняшний день модуль заточен в основном под Престашоп
-
-# """
-# MODE = 'dev'
-
-# from pathlib import Path
-# import os
-# import asyncio
-# from typing import List, Dict
-# from lxml import html
-# import requests
-
-# import header
-# from src import gs
-# from src.logger import logger 
-# from src.utils.jjson import j_loads_ns, j_dumps, pprint
-# from src.utils.string import StringFormatter
-# from src.endpoints.prestashop import PrestaShop
-# from src.endpoints.prestashop import PrestaCategory 
-
-
-# class Category(PrestaCategory):
-#     """ Класс категорий товара. Наследует `PrestaCategory` """
-
-#     credentials: dict = None
-
-#     def __init__(self, api_credentials, *args, **kwards):
-#         super().__init__(api_credentials, *args, **kwards)
-
-#     def get_parents(self, id_category, dept):
-#         """ Получение родительских категорий """
-#         return super().get_list_parent_categories(id_category)
-
-#     async def crawl_categories_async(self, url, depth, driver, locator, dump_file, id_category_default, category: dict = None):
-#         """ Асинхронная рекурсивная функция для обхода категорий и построения иерархического словаря.
-
-#         :param url: URL страницы для обхода.
-#         :param depth: Глубина рекурсии.
-#         :param driver: Экземпляр Selenium webdriver.
-#         :param locator: Xpath локатор для поиска ссылок на категории.
-#         :param dump_file: Файл для записи иерархического словаря.
-#         :param id_category_default: Идентификатор категории по умолчанию.
-#         :param category: Словарь, представляющий категорию, по умолчанию None.
-
-#         :return: Иерархический словарь, представляющий категории и их URL.
-#         """
-#         if category is None:
-#             category = {'url': url,
-#                         'name': '',
-#                         "presta_categories": {
-#                             "default_category": id_category_default,
-#                             "additional_categories": []
-#                         },
-#                         'children': {}}
-
-#         if depth <= 0:
-#             return category
-
-#         driver.get(url)
-#         driver.wait(1)
-#         category_links = driver.execute_locator(locator)
-#         if not category_links:
-#             logger.error(f"Что-то упало")
-#             ...
-#             return category
-
-#         tasks = []
-#         for link in category_links:
-#             for name, link_url in link.items():
-#                 if check_duplicate_url(category, link_url):
-#                     continue
-#                 new_category = {'url': link_url,
-#                                 'name': name,
-#                                 "presta_categories": {
-#                                     "default_category": id_category_default,
-#                                     "additional_categories": []
-#                                 },
-#                                 'children': {}}
-#                 task = self.crawl_categories_async(url=link_url,
-#                                                    depth=depth - 1,
-#                                                    driver=driver,
-#                                                    locator=locator,
-#                                                    dump_file=dump_file,
-#                                                    id_category_default=id_category_default,
-#                                                    category=new_category)
-#                 tasks.append(task)
-
-#         # Ждем завершения всех задач
-#         await asyncio.gather(*tasks)
-
-#         return category
-
-#     def crawl_categories(self, url, depth: int, driver, locator: dict, dump_file: Path, id_category_default, category: dict = {}):
-#         """ Рекурсивная функция для обхода категорий с сайта и построения иерархического словаря.
-
-#         :param url: URL страницы для обхода.
-#         :param depth: Глубина рекурсии.
-#         :param driver: Экземпляр Selenium webdriver.
-#         :param locator: Xpath локатор для поиска ссылок на категории.
-#         :param dump_file: Файл для записи иерархического словаря.
-#         :param id_category_default: Идентификатор категории по умолчанию.
-#         :param category: Словарь, представляющий категорию, по умолчанию пустой словарь.
-
-#         :return: Иерархический словарь, представляющий категории и их URL.
-#         """
-#         if depth <= 0:
-#             return category
-
-#         driver.get(url)
-#         driver.wait(1)
-#         category_links = driver.execute_locator(locator)
-#         if not category_links:
-#             ...
-#             return category
-
-#         for link in category_links:
-#             for name, link_url in link.items():
-#                 if check_duplicate_url(category, link_url):
-#                     continue
-#                 new_category = {
-#                     'url': link_url,
-#                     'name': name,
-#                     'presta_categories': {
-#                         "default_category": id_category_default,
-#                         "additional_categories": []
-#                     }
-#                 }
-#                 category[name] = new_category
-
-#                 self.crawl_categories(url=link_url,
-#                                       depth=depth - 1,
-#                                       driver=driver,
-#                                       locator=locator,
-#                                       dump_file=dump_file,
-#                                       id_category_default=id_category_default,
-#                                       category=new_category)
-#                 try:
-#                     dumped_dict: dict = j_loads(dump_file)
-#                     category = {**dumped_dict, **category}
-#                     j_dumps(category, dump_file)
-#                 except Exception:
-#                     ...
-
-#         j_dumps(category, dump_file)
-
-#         return category
-
-
-# def check_duplicate_url(dictionary, url) -> bool:
-#     """ Проверка, существует ли данный URL в иерархическом словаре.
-
-#     :param dictionary: Иерархический словарь для проверки.
-#     :param url: URL для проверки на дубли.
-
-#     :return: True, если URL уже существует, иначе False.
-#     """
-#     for key, value in dictionary.items():
-#         if key == 'url' and value == url:
-#             print(f"Category URL '{url}' уже существует.")
-#             return True
-#         for key, value in dictionary.get('children', {}).items():
-#             if key == 'url' and value == url:
-#                 print(f"Category URL '{url}' уже существует.")
-#                 return True
-
-#     return False
-
-
-# def compare_and_print_missing_keys(current_dict, file_path):
-#     """ Сравнение актуальных значений с теми, что в файле.
-
-#     :param current_dict: Текущий словарь для сравнения.
-#     :param file_path: Путь к файлу с данными для сравнения.
-
-#     Выводит ключи, которые отсутствуют в текущем словаре.
-#     """
-#     json_data = j_loads(file_path)
-
-#     # Пройти по всем ключам из файла JSON
-#     for key in json_data:
-#         # Если ключ уже существует в текущем словаре, пропустить его
-#         if key in current_dict:
-#             continue
-#         # Вывести ключ, если он не существует в текущем словаре
-#         print(key)
