b450134f786fe486433b89eac9f4c3a34d01aa87
hypo69 <181187165+hypo69@users.noreply.github.com>
2025-01-07 10:07:31 +0200

    added url_shortener

--- src/utils/url.py
+++ src/utils/url.py
@@ -6,14 +6,13 @@
 """
 .. module:: src.utils.string 
 	:platform: Windows, Unix
-	:synopsis: Модуль для работы с URL строками, включая извлечение параметров запроса и проверку на валидность UR
+	:synopsis: Модуль для работы с URL строками, включая извлечение параметров запроса, проверку на валидность URL и сокращение ссылок.
 
 """
 
-
-
 from urllib.parse import urlparse, parse_qs
 import validators
+import requests
 
 def extract_url_params(url: str) -> dict | None:
     """ Извлекает параметры из строки URL.
@@ -46,6 +45,23 @@ def is_url(text: str) -> bool:
     return validators.url(text)
 
 
+def url_shortener(long_url: str) -> str | None:
+    """ Сокращает длинный URL с использованием сервиса TinyURL.
+
+    Args:
+        long_url (str): Длинный URL для сокращения.
+
+    Returns:
+        str | None: Сокращённый URL или `None`, если произошла ошибка.
+    """
+    url = f'http://tinyurl.com/api-create.php?url={long_url}'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.text
+    return None
+
+
 if __name__ == "__main__":
     # Получаем строку URL от пользователя
     url = input("Введите URL: ")
@@ -61,5 +77,14 @@ if __name__ == "__main__":
                 print(f"{key}: {value}")
         else:
             print("URL не содержит параметров.")
+        
+        # Предлагаем пользователю сократить URL
+        shorten = input("Хотите сократить этот URL? (y/n): ").strip().lower()
+        if shorten == 'y':
+            short_url = url_shortener(url)
+            if short_url:
+                print(f"Сокращённый URL: {short_url}")
+            else:
+                print("Ошибка при сокращении URL.")
     else:
         print("Введенная строка не является валидным URL.")
